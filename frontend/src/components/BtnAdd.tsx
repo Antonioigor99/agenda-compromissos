6b5e68d50e3ba7e2444a3e340487666a2353b8b5
Antonio Igor <acardosomellojunior@gmail.com>
2025-05-29 21:03:40 -0300

    atualização pastas

--- frontend/src/components/BtnAdd.tsx
+++ frontend/src/components/BtnAdd.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
     type BtnAddProps = {
         onClick: () => void;
     }
@@ -6,4 +7,14 @@ const BtnAdd = ({onClick}:BtnAddProps) => {
         <button className="border text-center w-36 rounded-2xl" onClick={onClick}>Adicionar</button>
     )
 }
+=======
+    type BtnAddProps = {
+        onClick: () => void;
+    }
+const BtnAdd = ({onClick}:BtnAddProps) => {
+    return (
+        <button className="border text-center w-36 rounded-2xl" onClick={onClick}>Adicionar</button>
+    )
+}
+>>>>>>> 6366c8d (atualização pastas)
 export default BtnAdd
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 const Header = ():any=>{
     return (
     <header className=" w-full h-48 border">text</header>
 )
 }
+=======
+const Header = ():any=>{
+    return (
+    <header className=" w-full h-48 border">text</header>
+)
+}
+>>>>>>> 6366c8d (atualização pastas)
 export default Header
