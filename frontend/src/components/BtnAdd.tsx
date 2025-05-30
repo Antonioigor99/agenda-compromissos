9bae932ac950ff417de072493e448674532b0dd7
Antonio Igor Cardoso Mello Júnior <142550025+Antonioigor99@users.noreply.github.com>
2025-05-30 17:02:13 -0300

    Delete frontend directory

--- frontend/src/components/BtnAdd.tsx
+++
@@ -1,9 +0,0 @@
-    type BtnAddProps = {
-        onClick: () => void;
-    }
-const BtnAdd = ({onClick}:BtnAddProps) => {
-    return (
-        <button className="border text-center bg-blue-300 hover:bg-sky-700 w-36 rounded-2xl font-bold" onClick={onClick}>Adicionar</button>
-    )
-}
-export default BtnAdd
